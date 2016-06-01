8c617f7dd4e83a9c75a7d52b6621da7f6877f167
Rinnve <rinnve@yandex.ru>
2016-06-01 10:19:45 +0300

    Regex seem to be working

--- task2_1/solution.cpp
+++ task2_1/solution.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <regex>
 
+using namespace std;
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  string test1 = "2*x^100+100*x^2";
+  regex re(
+    R"_(([+-]){0,1}(\d*)\*(?:(x)(?:\^(\d*)){0,1}){0,1})_"
+  );
+
+  auto rstart = sregex_iterator(test1.begin(), test1.end(), re);
+  auto rend = sregex_iterator();
+  for (auto i = rstart; i != rend; i++)
+  {
+    smatch m = *i;
+    string m_str = m.str();
+    cout << m_str << endl;
+  }
   return 0;
 }
