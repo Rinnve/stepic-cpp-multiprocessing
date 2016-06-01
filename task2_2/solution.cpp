fac1f7125dd915802f34353745eb3279e6c045ea
Rinnve <rinnve@yandex.ru>
2016-06-01 19:37:50 +0300

    Task 2.2 complete

--- task2_2/solution.cpp
+++ task2_2/solution.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+class StringPointer {
+private:
+    std::string *s;
+public:
+    std::string *operator->() 
+    {
+        //if (s == NULL) s = new std::string();
+        return s;
+    }
+    std::string& operator*() 
+    {   
+        //if (s == NULL) s = new std::string();
+        return *s;
+    }
+    StringPointer(std::string *pointer)
+    {
+        s = (pointer == NULL) ? new std::string() : pointer;
+    }
+    ~StringPointer() 
+    {
+        //delete s;
+    }
+};
+
 int main()
 {
+    std::string s1 = "Hello, world!";
+
+    StringPointer sp1(&s1);
+    StringPointer sp2(NULL);
+
+    std::cout << sp1->length() << std::endl;
+    std::cout << *sp1 << std::endl;
+    std::cout << sp2->length() << std::endl;
+    std::cout << *sp2 << std::endl;
+    
     return 0;
 }
